d63421406adaaf975fefbd21b9fdb43062f6e926
charosqadirova13-hub <charosqadirova13@gmail.com>
2026-05-26 15:08:54 +0500

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
 import telebot
 from telebot import types
-import os
 from dotenv import load_dotenv
-from transliterate import to_cyrillic, to_latin
+import os
 
 load_dotenv()
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
+
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-  keyboard = types.ReplyKeyboardMarkup()
-  button1 = types.KeyboardButton('Men haqimda')
-  button2 = types.KeyboardButton('Loyihalarim')
-  keyboard.add(button1, button2)
-  text = "Assalom alaykum, botimizga xush kelibsiz \n quydagi tugmalardan birini tanlang"
-  bot.send_message(message.chat.id, text, reply_markup=keyboard)
+    keyboard = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton("Men haqimda")
+    btn2 = types.KeyboardButton("Loyihalarim")
+    keyboard.add(btn1, btn2)
+    text = "Assalom alaykum, men Asadbek Rakhimov. \nBu mening portfolio botim. \nQuyidagi bo'limlardan birini tanlang"
 
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-	print(message.text)
-  if message.text == 'Men haqimda':
-    bot.send_message(message.chat.id, "Men haqimda ma'lumot")
-  elif message.text == 'Loyihalarim':
-    bot.send_message(message.chat.id, "Loyihalarim haqida ma'lumot")
+    bot.send_message(message.chat.id, text, reply_markup=keyboard)
+
+@bot.message_handler(func=lambda m: m.text == "Men haqimda")
+def aboutme_handler(message):
+    text = "Men Asadbek frontend engineermen. React.js & Next.js specialistman"
+    
+    bot.send_message(message.chat.id, text)
+
+@bot.message_handler(func=lambda m: m.text == "Loyihalarim")
+def projects_handler(message):
+    text = "Bu qism tez orada qo'shiladi"
+    
+    bot.send_message(message.chat.id, text)
 	
 bot.infinity_polling()
-
-# s = input()
-# if s.isascii():
-#     print(to_cyrillic(s))
-# else:
-#     print(to_latin(s))
